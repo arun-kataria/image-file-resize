47f7926f4d731562927d9abb0f0c27445d791026
arun_kataria <arun.kataria@insurancedekho.com>
2020-04-10 14:17:42 +0530

    fist commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# image-file-resize
+ 
+This node module to resize the image file according to width and height. Also you can change the extension of a file.
+
+## Install
+
+```sh
+npm install --save image-file-resize
+```
+
+
---
+++ index.js
@@ -0,0 +1,31 @@
+function imageConvert({ file, width, height, imageType }) {
+    return new Promise(function (resolve, reject) {
+        let allow = ['jpg', 'gif', 'bmp', 'png', 'jpeg'];
+        if (file.name && allow.includes(file.name.split(".")[1]) && file.size && file.type && allow.includes(file.type.split("/")[1])) {
+            const imgWidth = width ? width : 500;
+            const imgHeight = height ? height : 300;
+            const fileName = file.name;
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = event => {
+                const img = new Image();
+                img.src = event.target.result;
+                img.onload = () => {
+                    const elem = document.createElement('canvas');
+                    elem.width = imgWidth;
+                    elem.height = imgHeight;
+                    const ctx = elem.getContext('2d');
+                    ctx.drawImage(img, 0, 0, imgWidth, imgHeight);
+                    ctx.canvas.toBlob((blob) => {
+                        const file = new File([blob], fileName, {
+                            type: 'image/jpeg',
+                            lastModified: Date.now()
+                        });
+                        resolve(file)
+                    }, 'image/jpeg', 1);
+                }, reader.onerror = error => reject(error);
+            };
+        } else reject('File not supported!')
+    })
+}
+export default imageConvert;
